e3f658c871049d8237622ada4b64d2ab5e461ce5
Bram Borggreve <beeman@beeman.nl>
2025-02-17 00:51:58 +0000

    Add vite-bundle-analyzer

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [nodePolyfills({ overrides: { fs: 'memfs' } }), react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
-  },
+    plugins: [
+        // analyzer(),
+        nodePolyfills({overrides: {fs: 'memfs'}}), react(), tsconfigPaths()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './vitest.setup.mjs',
+    },
 });
